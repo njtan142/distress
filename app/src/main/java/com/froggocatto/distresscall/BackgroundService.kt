b5db52def8630f73b2a7d02cf7dfeba9200f39ec
Niño James A. Tan <njtan142@gmail.com>
2024-02-24 01:14:09 +0800

    refactored backgroundservice class

--- app/src/main/java/com/froggocatto/distresscall/BackgroundService.kt
+++ app/src/main/java/com/froggocatto/distresscall/BackgroundService.kt
@@ -14,12 +14,14 @@ import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import com.mapbox.maps.plugin.annotation.annotations
 
 class BackgroundService : Service() {
 
     private val CHANNEL_ID = "DistressCallApp"
     private val NOTIFICATION_ID = 12
+    private lateinit var listener : ListenerRegistration
 
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -32,8 +34,6 @@ class BackgroundService : Service() {
     }
 
     private fun createNotificationChannel() {
-        // Create a notification channel for Android Oreo and higher
-        // Notification channels are required for foreground services
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 CHANNEL_ID,
@@ -56,40 +56,15 @@ class BackgroundService : Service() {
             notificationIntent,
             PendingIntent.FLAG_IMMUTABLE
         )
-
         val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Distress Alert Monitoring")
             .setContentText("Keeping an eye to incidents...")
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentIntent(pendingIntent)
             .build()
-
         startForeground(1, notification)
         Toast.makeText(this, "Foreground service started", Toast.LENGTH_SHORT).show()
-        getDistresses()
     }
-
-//    private fun showNotification(title: String, content: String) {
-//        val notificationIntent = Intent(this, MainActivity::class.java)
-//        val pendingIntent = PendingIntent.getActivity(
-//            this,
-//            0,
-//            notificationIntent,
-//            0
-//        )
-//
-//        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-//            .setContentTitle(title)
-//            .setContentText(content)
-//            .setSmallIcon(R.mipmap.ic_launcher)
-//            .setContentIntent(pendingIntent)
-//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//            .build()
-//
-//        with(NotificationManagerCompat.from(this)) {
-////            notify(NOTIFICATION_ID, notification)
-//        }
-//    }
 
     fun generateNotification(title: String, message: String) {
         val intent = Intent(this, MainActivity::class.java)
@@ -98,7 +73,6 @@ class BackgroundService : Service() {
         val pendingIntent = PendingIntent.getActivity(
             this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
         )
-
         var builder: NotificationCompat.Builder = NotificationCompat.Builder(
             applicationContext, channel_ID
         )
@@ -126,81 +100,35 @@ class BackgroundService : Service() {
 
         notificationManager.notify(0, builder.build())
     }
+
     fun getRemoteView(title:String, message: String): RemoteViews {
         val remoteView = RemoteViews("com.froggocatto.distresscall", R.layout.notification_layout)
-
         remoteView.setTextViewText(R.id.notification_title, title)
         remoteView.setTextViewText(R.id.notification_message, message)
-
         return remoteView
     }
 
     fun getDistresses() {
         val db = FirebaseFirestore.getInstance()
         val colRef = db.collection("distresses");
-//        colRef.get().addOnCompleteListener { task ->
-//            run {
-//                val snapshot = task.result
-//                documents = snapshot.documents
-//                for (documentSnapshot in documents!!) {
-//                    val data = documentSnapshot.data!!
-//                    val type = data["distress"].toString()
-//                    val resourceId = when (type) {
-//                        "Fire" -> R.drawable.fire_symbol
-//                        "Crime" -> R.drawable.crime
-//                        "Accident" -> R.drawable.accident_symbol
-//                        "Earthquake" -> R.drawable.earthquake
-//                        else -> {
-//                            R.drawable.other_symbol
-//                        }
-//                    }
-//
-//                    addAnnotationToMap(
-//                        data["longitude"].toString().toDouble(),
-//                        data["latitude"].toString().toDouble(),
-//                        resourceId
-//                    )
-//                }
-//            }
-//        }
-        val listener = colRef.addSnapshotListener { snapshot, e ->
+        listener = colRef.addSnapshotListener { snapshot, e ->
             if (e != null) {
                 // Handle errors
                 return@addSnapshotListener
             }
-
             if (snapshot != null) {
                 generateNotification("Alert!!!", "New messages")
-                // Clear existing annotations
-                // Process new data
-//                for (documentSnapshot in snapshot.documents) {
-//                    val data = documentSnapshot.data!!
-//                    val type = data["distress"].toString()
-//                    val resourceId = when (type) {
-//                        "Fire" -> R.drawable.fire_symbol
-//                        "Crime" -> R.drawable.crime
-//                        "Accident" -> R.drawable.accident_symbol
-//                        "Earthquake" -> R.drawable.earthquake
-//                        else -> R.drawable.other_symbol
-//                    }
-//
-//                    addAnnotationToMap(
-//                        data["longitude"].toString().toDouble(),
-//                        data["latitude"].toString().toDouble(),
-//                        resourceId
-//                    )
-//                }
             }
         }
 
     }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // TODO: Start your background tasks or services here.
+        getDistresses()
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        // TODO: Perform any cleanup or finalization here.
+        listener.remove()
     }
 }
